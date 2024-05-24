0b432067b37e78771d451180a88f2c7be56e97a7
Hari <hari@markhabadev.onmicrosoft.com>
2024-05-24 17:18:26 +0530

    design completed

--- app/src/main/java/com/example/flagschallange/screens/QuestionAndAnswer.kt
+++ app/src/main/java/com/example/flagschallange/screens/QuestionAndAnswer.kt
@@ -1,7 +1,11 @@
 package com.example.flagschallange.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -22,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,10 +38,11 @@ import com.example.flagschallange.ui.theme.AppColor
 
 @Composable
 fun QuestionAns() {
+    val context = LocalContext.current
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 15.dp, end = 15.dp, top = 15.dp),
+            .padding(start = 15.dp, end = 15.dp, top = 15.dp, bottom = 15.dp),
         RoundedCornerShape(5.dp),
         colors = CardDefaults.cardColors(AppColor.lightGray200)
     ) {
@@ -103,23 +109,105 @@ fun QuestionAns() {
                     .height(5.dp)
                     .fillMaxWidth()
             )
-            Card(
-                modifier = Modifier
-                    .width(100.dp)
-                    .height(80.dp)
-                    .padding(start = 10.dp),
-                colors = CardDefaults.cardColors(Color.LightGray)
-            ){
-                Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Card(
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(80.dp)
+                        .padding(start = 10.dp, bottom = 10.dp),
+                    colors = CardDefaults.cardColors(Color.LightGray)
+                ) {
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
 
-                    Image(
-                        painter = rememberImagePainter(data = R.drawable.nz),
-                        contentDescription = "", // Provide a meaningful description
+                        Image(
+                            painter = rememberImagePainter(data = R.drawable.nz),
+                            contentDescription = "", // Provide a meaningful description
 
-                        modifier = Modifier
-                            .width(70.dp)
-                            .height(50.dp)
-                    )
+                            modifier = Modifier
+                                .width(70.dp)
+                                .height(50.dp)
+                        )
+
+                    }
+
+
+                }
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Card(
+                            modifier = Modifier.clickable {
+                                Toast.makeText(context,"Clicked",Toast.LENGTH_LONG).show()
+                            },
+                            colors = CardDefaults.cardColors(AppColor.lightGray200),
+                            shape = RoundedCornerShape(5.dp),
+                            border = BorderStroke(1.dp, Color.DarkGray),
+                            elevation = CardDefaults.cardElevation(5.dp)
+                        ) {
+                            Text(
+                                text = "Colombo", modifier = Modifier.padding(
+                                    start = 20.dp, end = 20.dp,
+                                    top = 5.dp, bottom = 5.dp
+                                ), color = Color.Black,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        Card(
+                            modifier = Modifier,
+                            colors = CardDefaults.cardColors(AppColor.lightGray200),
+                            shape = RoundedCornerShape(5.dp),
+                            border = BorderStroke(1.dp, Color.DarkGray),
+                            elevation = CardDefaults.cardElevation(5.dp)
+                        ) {
+                            Text(
+                                text = "Colombo", modifier = Modifier.padding(
+                                    start = 20.dp, end = 20.dp,
+                                    top = 5.dp, bottom = 5.dp
+                                ), color = Color.Black,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(10.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Card(
+                            modifier = Modifier,
+                            colors = CardDefaults.cardColors(AppColor.lightGray200),
+                            shape = RoundedCornerShape(5.dp),
+                            border = BorderStroke(1.dp, Color.DarkGray),
+                            elevation = CardDefaults.cardElevation(5.dp)
+                        ) {
+                            Text(
+                                text = "Colombo", modifier = Modifier.padding(
+                                    start = 20.dp, end = 20.dp,
+                                    top = 5.dp, bottom = 5.dp
+                                ), color = Color.Black,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        Card(
+                            modifier = Modifier,
+                            colors = CardDefaults.cardColors(AppColor.lightGray200),
+                            shape = RoundedCornerShape(5.dp),
+                            border = BorderStroke(1.dp, Color.DarkGray),
+                            elevation = CardDefaults.cardElevation(5.dp)
+                        ) {
+                            Text(
+                                text = "Colombo", modifier = Modifier.padding(
+                                    start = 20.dp, end = 20.dp,
+                                    top = 5.dp, bottom = 5.dp
+                                ), color = Color.Black,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
 
                 }
             }
