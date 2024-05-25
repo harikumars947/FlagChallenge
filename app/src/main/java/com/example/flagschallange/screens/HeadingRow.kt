b1bb32a86f43544fad230c78088d73bef761561c
Hari <hari@markhabadev.onmicrosoft.com>
2024-05-25 21:59:08 +0530

    UI implenetd

--- app/src/main/java/com/example/flagschallange/screens/HeadingRow.kt
+++ app/src/main/java/com/example/flagschallange/screens/HeadingRow.kt
@@ -1,7 +1,9 @@
 package com.example.flagschallange.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -14,11 +16,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.flagschallange.R
 import com.example.flagschallange.ui.theme.AppColor
 import com.example.flagschallange.viewmodels.QuestionAnswerViewModel
 
@@ -30,27 +34,35 @@ fun Heading(viewModel: QuestionAnswerViewModel?){
         modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Card(
-            modifier = Modifier
-                .height(20.dp)
-                .width(50.dp), colors = CardDefaults.cardColors(Color.Black),
-            shape = RoundedCornerShape(5.dp),
-            elevation = CardDefaults.cardElevation(5.dp)
-        ) {
+        Box() {
+            Card(
+                modifier = Modifier
+                    .height(dimensionResource(R.dimen._30dp))
+                    .width(dimensionResource(R.dimen._50dp)), colors = CardDefaults.cardColors(Color.Black),
+                shape = RoundedCornerShape(dimensionResource(R.dimen._5dp)),
+                elevation = CardDefaults.cardElevation(dimensionResource(R.dimen._5dp))
+            )
+
+            {
+                Box(modifier = Modifier.fillMaxHeight(),contentAlignment = Alignment.Center) {
+                    Text(
+                        text = "${timeLeft?.value?:"00:00"}", modifier = Modifier
+                            .fillMaxWidth(), color = Color.White,
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+            }
             Text(
-                text = "${timeLeft?.value?:"00:00"}", modifier = Modifier
-                    .fillMaxWidth(), color = Color.White,
-                textAlign = TextAlign.Center
+                "Flag Challenge",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontSize = dimensionResource(R.dimen._20sp).value.sp, color = AppColor.OrangeColor,
+                fontWeight = FontWeight.Bold
             )
         }
-        Text(
-            "Flag Challenge",
-            modifier = Modifier
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            fontSize = 14.sp, color = AppColor.OrangeColor,
-            fontWeight = FontWeight.Bold
-        )
+
 
     }
 }
